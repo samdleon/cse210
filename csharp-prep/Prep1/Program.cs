9b72e6cbce065c825cfe3d090914c7361bb0cc5b
Sam Leon <leo19001@byui.edu>
2026-09-18 20:07:42 -0600

    completed prep1 and prep2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+
+    Console.Write("\nWhat is your first name? ");
+    string fname = Console.ReadLine();
+
+    Console.Write("What is your last name? ");
+    string lname = Console.ReadLine();
+
+    Console.WriteLine($"\nYour name is {lname}, {fname} {lname}.\n");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,64 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+
+    string scoreText;
+    int scoreNum;
+    string letterGrade;
+
+    Console.Write("\nWhat is your grade percentage? ");
+    scoreText = Console.ReadLine();
+    scoreNum = int.Parse(scoreText);
+
+    if (scoreNum >= 90)
+        {
+            letterGrade = "A";
+        }
+    
+    else if (scoreNum >= 80)
+        {
+            letterGrade = "B";
+        }
+
+    else if (scoreNum >= 70)
+        {
+            letterGrade = "C";
+        }
+
+    else if (scoreNum >= 60)
+        {
+            letterGrade = "D";
+        }
+
+    else
+        {
+            letterGrade = "F";
+        }
+
+
+    Console.WriteLine($"Your letter grade is {letterGrade}.");
+
+
+    if (letterGrade != "D" && letterGrade != "F") // grade must not be a D or F to pass
+        {
+            Console.WriteLine("Congratulations, you passed!\n");
+        }
+
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass. Better luck next time!\n");
+        }
+
+
+
+
     }
+
+
+
 }
